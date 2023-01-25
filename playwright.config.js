fb5be52a1d0c192045b41cdc716868c1b046cfbf
Alex Trofimov <alex.trofimov@boonex.com>
2023-01-25 23:16:12 +1100

    make screenshot on fail

--- playwright.config.js
+++ playwright.config.js
@@ -40,11 +40,14 @@ const config = {
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
+
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env?.baseUrl ? process.env.baseUrl : 'http://localhost',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -100,7 +103,7 @@ const config = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
