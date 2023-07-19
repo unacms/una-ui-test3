a264ea7b8d73c63b7de9f4f86cd86758b5de5d89
Alex Trofimov <alex.trofimov@boonex.com>
2023-07-19 14:24:05 +0600

    Refactoring

--- fixtures/bx_posts/sample.js
+++ fixtures/bx_posts/sample.js
@@ -5,6 +5,6 @@ module.exports = {
         { name: "cat", select: 3 }, // Art
         { name: "text", val: "<p>Sample content</p><p>new line</p>", "eval": true }, // eval is needed to force set value to invisible element
     ],
-    "cover": "fixtures/posts/sample_cover.jpg",
-    "photo_attachment": "fixtures/posts/sample_attachment.jpg",
+    "cover": "fixtures/bx_posts/sample_cover.jpg",
+    "photo_attachment": "fixtures/bx_posts/sample_attachment.jpg",
 }
