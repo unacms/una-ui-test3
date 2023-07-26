98b6078fc2d05c4d815a3e4a5e0850884b363147
kisonik <kisonik@gmail.com>
2023-07-26 16:52:27 +0600

    Account Creation
    Account Creation Test

---
+++ fixtures/system/accounts.js
@@ -0,0 +1,68 @@
+module.exports = {
+    "data": [
+        [
+            { name: "name", val: "tom" },
+            { name: "email", val: "tom@example.com" },
+        ],
+        [
+            { name: "name", val: "ivan" },
+            { name: "email", val: "ivan@example.com" },
+        ],
+        [
+            { name: "name", val: "karen" },
+            { name: "email", val: "karen@example.com" },
+        ],
+        [
+            { name: "name", val: "rob" },
+            { name: "email", val: "rob@example.com" },
+        ],
+        [
+            { name: "name", val: "viky" },
+            { name: "email", val: "viky@example.com" },
+        ],
+        [
+            { name: "name", val: "eva" },
+            { name: "email", val: "eva@example.com" },
+        ],
+        [
+            { name: "name", val: "eric" },
+            { name: "email", val: "eric@example.com" },
+        ],
+        [
+            { name: "name", val: "emma" },
+            { name: "email", val: "emma@example.com" },
+        ],
+        [
+            { name: "name", val: "jack" },
+            { name: "email", val: "jack@example.com" },
+        ],
+        [
+            { name: "name", val: "ella" },
+            { name: "email", val: "ella@example.com" },
+        ],
+        [
+            { name: "name", val: "lily" },
+            { name: "email", val: "lily@example.com" },
+        ],
+        [
+            { name: "name", val: "dave" },
+            { name: "email", val: "dave@example.com" },
+        ],
+        [
+            { name: "name", val: "mila" },
+            { name: "email", val: "mila@example.com" },
+        ],
+        [
+            { name: "name", val: "linda" },
+            { name: "email", val: "linda@example.com" },
+        ],
+        [
+            { name: "name", val: "andy" },
+            { name: "email", val: "andy@example.com" },
+        ],
+        [
+            { name: "name", val: "luck" },
+            { name: "email", val: "luck@example.com" },
+        ]
+    ]
+}
---
+++ tests/system/create-accounts.spec.js
@@ -0,0 +1,44 @@
+// vi: set ts=2 sw=2 sts=2:
+
+const { test } = require('@playwright/test');
+const { fillFormInput } = require('../../lib/util.js');
+const Accounts = require("../../fixtures/system/accounts");
+
+// loop through Create Account tests
+for (let i = 0; i < Accounts.data.length; i++) {
+
+    test.describe('Accounts_' + i, () => {
+
+        test('Should allow create accounts', async ({ page }) => {
+
+            // before each
+            await page.context().clearCookies();
+            await page.goto('create-account');
+
+            // fill in name, email
+            for (const [, row] of Accounts.data[i].entries()) {
+                await fillFormInput(page, row);
+            };
+
+            // fill in password
+            await page.locator('input[name="password"]').fill(process.env.userTomPwd);
+
+            // Click Submit
+            await page.getByRole('button', { name: 'Submit' }).click();
+
+            // check if account exists
+            const isAccountExists = await page.evaluate(() => !document.querySelector('#bx-form-element-email .bx-form-warn'));
+            if (isAccountExists) {
+                return;
+            }
+
+            // click logout button
+            await page.locator('#bx-menu-toolbar-item-account a').click();
+            await page.locator('li.bx-menu-item-logout a').click();
+
+        });
+
+    });
+
+};
+
