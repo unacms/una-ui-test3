58a56de6cdbb65dc786bb696f2e998604cbe1e58
LeonidSokushev <ls@una.io>
2023-08-03 15:00:37 +0600

    The friends connections
    The friends connections for the current profiles (the view profile links are set for ci.una.io/test site)

---
+++ fixtures/system/friends.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "data": [
+        {
+          "email": "admin@example.com",
+          "name" : "John Doe",
+          "list" : ["Emily Smith", "Sam Doe Senior", "Jack Doe Junior"],
+        },
+        {
+          "email": "rob@example.com",
+          'name': "Sam Doe Senior",
+          "list": ["John Doe", "Victoria Chen", "Emily Smith", "Tim Doe, MD. PhD.", "Jack Doe Junior", "Jane Doe"]
+        },
+        {
+          "email": "jack@example.com",
+          "name" : "Jack Doe Junior",
+          "list" : ["John Doe", "Sam Doe Senior"],
+        },
+        {
+          "email": "eva@example.com",
+          "name" : "Jane Doe",
+          "list" : ["Sam Doe Senior"],
+        },
+        {
+          "email": "viky@example.com",
+          'name': "Victoria Chen",
+          "list": ["Sam Doe Senior"]
+        },
+        {
+          "email": "luck@example.com",
+          "name" : "Tim Doe, MD. PhD.",
+          "list" : ["Sam Doe Senior"],
+        },
+        {
+          "email": "emma@example.com",
+          'name': "Emily Smith",
+          "list": ["John Doe", "Sam Doe Senior"]
+        }
+      ]
+}
